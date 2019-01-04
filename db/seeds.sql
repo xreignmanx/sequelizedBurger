8a4690a78440888ac2efeb5e7812dd05b27a33be
xreignmanx <rtballentine@yahoo.com>
2019-01-04 09:28:53 -0500

    init commit

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Big Red');
+INSERT INTO burgers (burger_name) VALUES ('Avacado');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Gouda', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hammy', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie', true);
+INSERT INTO burgers (burger_name) VALUES ('Bunion');
